b7bd9e6c284a15af94a58e15c0eac2925ba3da2c
Shedlia Freeman <sfreeman@basecampcodingacademy.org>
2018-03-01 02:37:47 -0600

    Implemented gladiator function
    I made a gladiator function that I can reuse for future gladiators

--- src/clojuregladiators/core.clj
+++ src/clojuregladiators/core.clj
@@ -1,19 +1,24 @@
 (ns clojuregladiators.core
   (:gen-class))
 
-
-(defn -main
-  [& args]
-  (defn gladiator [name] 
+(defn gladiator 
+"A map instance of a gladiator with the following parameters:\n\t-health
+\n\t-rage\n\t-name\n\t-attack low\n\t-attack high"[name]
+ 
   {:health 100
   :rage 0
   :name name
   :attackLow 10
   :attackHigh 25})
+
+(defn -main
+  [& args]
+  (println "Player One Name: ")
   (def gladiatorOneName (read-line))
+  (println "Player Two Name: ")  
   (def gladiatorTwoName (read-line)) 
-  (def gladiatorOne (gladiator "Shedlia"))
-  (def gladiatorTwo (gladiator "Shelly"))
+  (def gladiatorOne (gladiator gladiatorOneName))
+  (def gladiatorTwo (gladiator gladiatorTwoName))
   (println "Gladiator One Name is" gladiatorOneName "and gladiator Two name is" gladiatorTwoName)
   (println (get gladiatorOne :name))
   )
